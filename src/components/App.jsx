51e7e44f6605dc26df2e6002533853f721f6453b
Pablio <pablio@linux-vct1.vtr.cl>
2018-06-04 18:27:56 -0400

    wea

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,64 +6,77 @@ import { Header, Aside, DataSection } from "./layouts";
 import DataLoader from "./DataLoader";
 
 const styles = theme => ({
-	app: {
-		height: "100%",
-		display: "flex",
-		flexDirection: "column",
-	},
-	paper: {
-		flex: 1,
-	},
-	container: {
-		height: "100%",
-	},
-	gridItemDataSection: {
-		display: "flex",
-		flexDirection: "column",
-	},
+    app: {
+        height: "100%",
+        display: "flex",
+        flexDirection: "column"
+    },
+    paper: {
+        flex: 1
+    },
+    container: {
+        height: "100%"
+    },
+    gridItemDataSection: {
+        display: "flex",
+        flexDirection: "column"
+    }
 });
-
-const DataSectionLoader = DataLoader(
-	DataSection,
-	"https://ipapi.co/200.83.2.5/json/",
-	[],
-	data => {
-		return {
-			markers: [
-				{
-					lat: data.latitude,
-					lng: data.longitude,
-				},
-			],
-		};
-	},
-);
 
 // const DataSectionLoader = DataLoader(
 // 	DataSection,
-// 	"https://apis.digital.gob.cl/dpa/comunas",
-// 	[],
-// 	data => data.map(c => ({lat: c.lat, lng: c.lng}))
+// 	"https://ipapi.co/200.83.2.5/json/",
+// 	data => {
+// 		return {
+// 			markers: [
+// 				{
+// 					lat: data.latitude,
+// 					lng: data.longitude,
+// 				},
+// 			],
+// 		};
+// 	},
 // );
 
+const DataSectionLoader = DataLoader(
+    DataSection,
+    "http://desannoc.vtr.cl/pmenares/emap/controller.php?mod=test",
+    data => {
+        return {
+            markers: data.map(c => ({ lat: c.lat, lng: c.lng }))
+        };
+    }
+);
+
+const AsideLoader = DataLoader(
+    Aside,
+    "http://desannoc.vtr.cl/pmenares/emap/controller.php?mod=fen",
+    data => {
+        return {
+            events: data,
+            eventsType: "fen"
+        };
+    }
+);
+
 const App = props => {
-	const { classes } = props;
-	return (
-		<div className={classes.app}>
-			<CssBaseline />
-			<Header />
-			<Paper square={false} className={classes.paper}>
-				<Grid container className={classes.container}>
-					<Grid item xs={4} style={{ maxWidth: 350 }}>
-						<Aside />
-					</Grid>
-					<Grid item xs className={classes.gridItemDataSection}>
-						<DataSectionLoader {...props} />
-					</Grid>
-				</Grid>
-			</Paper>
-		</div>
-	);
+    const { classes } = props;
+    return (
+        <div className={classes.app}>
+            <CssBaseline />
+            <Header />
+            <Paper square={false} className={classes.paper}>
+                <Grid container className={classes.container}>
+                    <Grid item xs={4} style={{ maxWidth: 350 }}>
+                        <Aside />
+                    </Grid>
+                    <Grid item xs className={classes.gridItemDataSection}>
+                        <DataSectionLoader />
+                    </Grid>
+                </Grid>
+            </Paper>
+        </div>
+    );
 };
 
 export default withStyles(styles)(App);
--- src/components/DataLoader.jsx
+++ src/components/DataLoader.jsx
@@ -1,43 +1,52 @@
 import React from "react";
 import axios from "axios";
 
-const DataLoader = (Composed, url, params, parse) =>
-	class DataLoader extends React.Component {
-		constructor(props) {
-			super(props);
-			this.state = {
-				data: [],
-				loading: false,
-			};
-		}
+const DataLoader = (Composed, url, parse) =>
+    class DataLoader extends React.Component {
+        constructor(props) {
+            super(props);
+			const {load, params} = props;
+            this.state = {
+                loading: false,
+				data: []
+            };
+			this.fetch = this.fetch.bind(this);
+        }
 
-		componentWillMount() {
+        componentDidMount() {
+			console.log("props:", this.props);
+            //this.fetch();
 			this.setState({ loading: true });
-			axios
-				.get(url)
-				.then(result => {
-					console.log("api call succeed");
-					const parsed = parse ? parse(result.data) : result.data;
-					console.log("parsed:", parsed);
-					this.setState({
-						data: parsed,
-						loading: false,
-					});
-				})
-				.catch(error => {
-					console.log("!!", error);
-					this.setState({
-						loading: false,
-						loadError: error,
-					});
-				});
-		}
+			setTimeout(this.fetch, 1000);
+        }
 
-		render() {
-			return (
-				<Composed loading={this.state.loading} {...this.state.data} />
-			);
-		}
-	};
+        fetch(params) {
+            this.setState({ loading: true });
+            axios
+                .get(url)
+                .then(result => {
+                    console.log("api call succeed");
+                    const parsed = parse ? parse(result.data) : result.data;
+                    console.log("parsed:", parsed);
+                    this.setState({
+                        data: parsed,
+                        loading: false
+                    });
+                })
+                .catch(error => {
+                    console.log("!!", error);
+                    this.setState({
+                        loading: false,
+                        loadError: error
+                    });
+                });
+        }
+
+        render() {
+            return (
+                <Composed loading={this.state.loading} {...this.state.data} />
+            );
+        }
+    };
 
 export default DataLoader;
