3336d8b22ff907cf149e024cc2bd52d540f1cbb1
Pablio <pablio@linux-vct1.vtr.cl>
2018-06-05 18:22:31 -0400

    wea2

--- src/components/EventPicker.jsx
+++ src/components/EventPicker.jsx
@@ -7,6 +7,7 @@ import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
+import EventsTable from "./EventsTable";
 
 export default class EventPicker extends React.Component {
 	state = {
@@ -32,19 +33,7 @@ export default class EventPicker extends React.Component {
 						Selección de Eventos
 					</DialogTitle>
 					<DialogContent>
-						<DialogContentText>
-							To subscribe to this website, please enter your
-							email address here. We will send updates
-							occasionally.
-						</DialogContentText>
-						<TextField
-							autoFocus
-							margin="dense"
-							id="name"
-							label="Email Address"
-							type="email"
-							fullWidth
-						/>
+						<EventsTable />
 					</DialogContent>
 					<DialogActions>
 						<Button onClick={this.handleClose} color="primary">
